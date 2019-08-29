d4c3098e573a7a66c32708fabe6a955d924f503b
Kingsella <jmusa945@gmail.com>
2019-08-29 11:37:41 +0300

    first commit

---
+++ lib/countdown_web/controllers/page_controller.ex
@@ -0,0 +1,10 @@
+defmodule CountdownWeb.PageController do
+  use CountdownWeb, :controller
+
+  alias Countdown.Events
+
+  def index(conn, _params) do
+    events = Events.list_future_events()
+    render(conn, "index.html", events: events)
+  end
+end
